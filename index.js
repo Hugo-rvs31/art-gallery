ffccf97c10d963e315caabd07b5dcc6db5d49cb2
Hugo-rvs31 <hug.rvs31@gmail.com>
2025-04-21 19:04:51 +0200

    ajout 12

--- index.js
+++ index.js
@@ -13,3 +13,83 @@ window.addEventListener("scroll", () => {
 
   lastScroll = window.scrollY;
 });
+
+const container1 = document.querySelector(".container-1");
+const container2 = document.querySelector(".container-2");
+const barreLeft = document.querySelector(".barre-on");
+const barreRight = document.querySelector(".barre-off");
+const popUpConversation = document.getElementById("pop-up-conversation");
+const popUpInner = document.getElementById("pop-up-inner");
+const LeavePopUp = document.querySelector(".leave-pop-up");
+const header1 = document.querySelector(".header-1");
+const header2 = document.querySelector(".header-2");
+const header3 = document.querySelector(".header-3");
+
+barreLeft.addEventListener("click", () => {
+  container1.style.visibility = "visible";
+  container2.style.visibility = "hidden";
+  barreLeft.style.background = "black";
+  barreRight.style.background = "rgb(205 202 202 / 92%)";
+  console.log("gvhjvj");
+});
+
+barreRight.addEventListener("click", () => {
+  container2.style.visibility = "visible";
+  container1.style.visibility = "hidden";
+  barreRight.style.background = "black";
+  barreLeft.style.background = "rgb(205 202 202 / 92%)";
+  console.log("dgfdh");
+});
+
+popUpConversation.addEventListener("click", () => {
+  popUpInner.style.visibility = "visible";
+});
+
+LeavePopUp.addEventListener("click", () => {
+  popUpInner.style.visibility = "hidden";
+});
+
+barre1.addEventListener("click", () => {
+  header1.style.visibility = "visible";
+  header2.style.visibility = "hidden";
+  header3.style.visibility = "hidden";
+  barre1.style.background = "white";
+  barre1.style.height = "6px";
+  barre1.style.width = "32px";
+  barre2.style.background = "grey";
+  barre2.style.height = "3px";
+  barre2.style.width = "20px";
+  barre3.style.background = "grey";
+  barre3.style.height = "3px";
+  barre3.style.width = "20px";
+});
+
+barre2.addEventListener("click", () => {
+  header1.style.visibility = "hidden";
+  header3.style.visibility = "hidden";
+  header2.style.visibility = "visible";
+  barre2.style.background = "white";
+  barre2.style.height = "6px";
+  barre2.style.width = "32px";
+  barre1.style.background = "grey";
+  barre1.style.height = "3px";
+  barre1.style.width = "20px";
+  barre3.style.background = "grey";
+  barre3.style.height = "3px";
+  barre3.style.width = "20px";
+});
+
+barre3.addEventListener("click", () => {
+  header1.style.visibility = "hidden";
+  header2.style.visibility = "hidden";
+  header3.style.visibility = "visible";
+  barre3.style.background = "white";
+  barre3.style.height = "6px";
+  barre3.style.width = "32px";
+  barre2.style.background = "grey";
+  barre2.style.height = "3px";
+  barre2.style.width = "20px";
+  barre1.style.background = "grey";
+  barre1.style.height = "3px";
+  barre1.style.width = "20px";
+});
